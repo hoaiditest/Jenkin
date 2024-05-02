a468dd5314a4ef14eb0646d620652258d4cfc155
Duy.Pro <nesv025@gmail.com>
2024-05-02 13:32:44 +0700

    di1

---
+++ tests/example.spec.js
@@ -0,0 +1,10 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+
+test("has title", async ({ page }) => {
+  await page.goto("https://customer.engibase.com/");
+  await page.locator("#email").fill("nesv025@learningift.com");
+  await page.locator("#password").fill("Duywasd123");
+  await page.locator("#btn-login").click();
+  await page.waitForTimeout(2000);
+});
