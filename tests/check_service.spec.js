b091a75a47f91bd51b89833cb86b266f943e1c66
Duy.Pro <nesv025@gmail.com>
2024-11-26 16:00:12 +0700

    create function sent mail and check_class and call function

--- tests/check_service.spec.js
+++ tests/check_service.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
+import { check_class, sentmail } from "./z_function";
 const { chromium } = require("playwright");
-const nodemailer = require("nodemailer");
 
 test("test", async ({}) => {
   test.setTimeout(240000);
@@ -12,48 +12,17 @@ test("test", async ({}) => {
     },
   });
   const page = await context.newPage();
-  await page.setViewportSize({ width: 1920, height: 1080 });
   await page.goto("https://mta.engibase.com/haproxy?stats");
-  await page.waitForTimeout(3000);
-  await sendmail();
-  await page.waitForTimeout(3000);
-  /*await page.goto('https://ha.v2.engibase.com/haproxy?stats');
-  await page.waitForTimeout(3000);
-  await sendmail();
-  await page.waitForTimeout(3000);*/
-
-  async function sendmail() {
-    const url = await page.url();
-    console.log("url:", url);
-    const element = await page.$$(
-      '//tr[@class="active_down"]//a[@class="lfsb"]'
+  const url = await page.url();
+  const di = await check_class(page);
+  if (di.length > 0) {
+    await sentmail(
+      "active_down",
+      `active_down : \n
+      ${di} \n
+      url : ${url}\n
+      ID: nesv006 \n
+      PW: Jilv120312 \n`
     );
-    const active = element.length;
-    console.log("Số lượng có trên trang web:", active);
-    for (const d of element) {
-      const text = await d.textContent();
-      console.log("Văn bản của phần tử:", text);
-      //send mail
-      const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-          // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-          user: "dimot111111@gmail.com",
-          pass: "usahmoepwxiaysop",
-        },
-      });
-      const info = await transporter.sendMail({
-        from: '"From DiTest" <foo@example.com>', // sender address
-        to: "hoaiditest@gmail.com", // list of receivers
-        subject: `Title : ${text} active_down`, // Subject line
-        text: `Content : \n
-          ${text} active_down \n
-          url : ${url} \n 
-          ID: nesv006 \n  
-          PW: Jilv120312  `,
-      });
-    }
   }
 });
---
+++ tests/z_function.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+async function sentmail(title, content) {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "dimot111111@gmail.com",
+      pass: "drvm wsqm bjgk ispi ",
+    },
+  });
+  const info = await transporter.sendMail({
+    to: "hoaiditest@gmail.com",
+    subject: `${title}`,
+    text: `${content}`,
+  });
+}
+async function check_class(page) {
+  await page.waitForTimeout(3000);
+  const element = await page.$$(".active_down .lfsb");
+  const vps = [];
+  for (const d of element) {
+    const text = await d.textContent();
+    vps.push(text + "\n");
+  }
+  return vps;
+}
+module.exports = {
+  sentmail,
+  check_class,
+};
