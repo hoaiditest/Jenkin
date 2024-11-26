612263bcaec4cd6ea83bb5c3610a80639e3ebf72
Duy.Pro <nesv025@gmail.com>
2024-11-26 16:14:23 +0700

    Jpsic Env : Test Status

--- tests/check_service.spec.js
+++ tests/check_service.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 import { check_class, sentmail } from "./z_function";
 const { chromium } = require("playwright");
 
-test("test", async ({}) => {
+test("Check vps", async ({}) => {
   test.setTimeout(240000);
   const browser = await chromium.launch();
   const context = await browser.newContext({
@@ -23,6 +23,17 @@ test("test", async ({}) => {
       url : ${url}\n
       ID: nesv006 \n
       PW: Jilv120312 \n`
+    );
+  }
+});
+test("Jpsic ", async ({ page }) => {
+  await page.goto("https://test.jpsic.co.jp/");
+  await page.waitForTimeout(5000);
+  const response = await page.request.get(page.url());
+  if (response.status() == 200) {
+    await sentmail(
+      "Jpsic Env : Test Status khác 200",
+      "https://test.jpsic.co.jp/ Status khác 200"
     );
   }
 });
