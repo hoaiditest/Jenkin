03cbf9565d27b1cff1f3bb5428a98ff3bb89331b
Duy.Pro <nesv025@gmail.com>
2025-04-29 09:57:05 +0700

    Check vps : add url

--- tests/check_service.spec.js
+++ tests/check_service.spec.js
@@ -11,27 +11,34 @@ const { chromium } = require("playwright");
 
 test("Check vps", async ({}) => {
   test.setTimeout(240000);
-  const browser = await chromium.launch();
-  const context = await browser.newContext({
-    httpCredentials: {
-      username: "nesv006",
-      password: "Jilv120312",
-    },
-  });
-  const page = await context.newPage();
-  await page.goto("https://mta.engibase.com/haproxy?stats");
-  const url = await page.url();
-  const di = await check_class(page);
-  if (di.length > 0) {
-    await sentmail_error(
-      page,
-      `active_down  : ${new Date().toLocaleString()}`,
-      `active_down  : ${new Date().toLocaleString()} : \n
+  let link = [
+    "https://mta.engibase.com/haproxy?stats",
+    "https://pcw.v1.engibase.com/haproxy?stats",
+  ];
+  for (let i = 0; i < link.length; i++) {
+    const browser = await chromium.launch();
+    const context = await browser.newContext({
+      httpCredentials: {
+        username: "nesv006",
+        password: "Jilv120312",
+      },
+    });
+    const page = await context.newPage();
+    await page.goto(link[i]);
+    const url = await page.url();
+    const di = await check_class(page);
+    if (di.length > 0) {
+      await sentmail_error(
+        page,
+        `active_down  : ${new Date().toLocaleString()}`,
+        `active_down  : ${new Date().toLocaleString()} : \n
       ${di} \n
       url : ${url}\n
       ID: nesv006 \n
       PW: Jilv120312 \n`
-    );
+      );
+    }
+    await browser.close();
   }
 });
 test("Jpsic ", async ({ page }) => {
