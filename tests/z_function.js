15b748ee6c5ba4d6cab7e92a276ebac6dda75670
DUY\hoaid <nesv025@gmail.com>
2025-09-15 10:02:31 +0700

    click_menu

--- tests/z_function.js
+++ tests/z_function.js
@@ -1596,10 +1596,7 @@ async function click_menu(page) {
   await console.log(menu.length);
   for (let a = 0; a < menu.length; a++) {
     // await console.log(menu[a]);
-    if (
-      menu_customer_InnerTexts[a] != "ログアウト" &&
-      menu_customer_InnerTexts[a] != "dimot111111@gmail.com"
-    ) {
+    if (menu_customer_InnerTexts[a] != "ログアウト" && a != 42) {
       const url1 = await page.url();
       await page.waitForTimeout(1000);
       await menu[a].click({ timeout: 10000 });
