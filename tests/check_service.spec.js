1135ba87139ea370a604cbcb0a32c016f78fb043
DUY\hoaid <nesv025@gmail.com>
2025-11-25 15:15:34 +0700

    Engibase_DaiLyLogin

--- tests/check_service.spec.js
+++ tests/check_service.spec.js
@@ -51,7 +51,64 @@ test("Check_VPS", async ({}) => {
   }
   console.log(`[${new Date().toLocaleString()}] End Check_VPS timeZone VN\n`);
 });
-
+test("Engibase_DaiLyLogin ", async ({ page }) => {
+  test.setTimeout(300000);
+  console.log(
+    `[${new Date().toLocaleString()}] Start Engibase_DaiLyLogin timeZone VN\n`
+  );
+  async function mail(page, url, response) {
+    await sentmail_error(
+      page,
+      `${url} に問題が発生しています ${new Date().toLocaleString("ja-JP", {
+        timeZone: "Asia/Tokyo",
+      })}`,
+      ` 
+      ${url} で問題が発生しています。
+      サポートをお願い致します。 
+      お客様のテスト環境はシャットダウンされています。
+      Status Code: ${response}
+      発生日時： ${new Date().toLocaleString("ja-JP", {
+        timeZone: "Asia/Tokyo",
+      })}(Asia/Tokyo)
+      発生日時：${new Date().toLocaleString()} (VN)
+      `,
+      [
+        "h-inui@learningift.com",
+        "nesv006@gmail.com",
+        "hayashi-y@learningift.com",
+      ]
+    );
+  }
+  async function Engibase_DaiLyLogin(page) {
+    await page.goto("https://visitor.engibase.com/");
+    await page.waitForTimeout(3000);
+    const response1 = await page.request.get(page.url());
+    const url1 = await page.url();
+    if (response1.status() != 200) {
+      await mail(page, url1, response1.status());
+    }
+    await login_all(
+      page,
+      "https://visitor.engibase.com/",
+      "duy@g-root.com",
+      "Duywasd123"
+    );
+    await page.waitForTimeout(3000);
+    const response = await page.request.get(page.url());
+    const url = await page.url();
+    if (response.status() != 200) {
+      await mail(page, url, response.status());
+    }
+  }
+  try {
+    await Engibase_DaiLyLogin(page);
+  } catch (error) {
+    await sentmail_error(page, `Engibase_DaiLyLogin ${error}`, `${error}`);
+  }
+  console.log(
+    `[${new Date().toLocaleString()}] End Engibase_DaiLyLogin timeZone VN\n`
+  );
+});
 test("Engibase ", async ({ page }) => {
   test.setTimeout(3600000);
   async function Check_Engibase(page) {
